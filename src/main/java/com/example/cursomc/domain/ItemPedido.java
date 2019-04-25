5378f86ffb3556d69e9b67823db49793be8c3698
JBraz <maxull@yahoo.com.br>
2019-04-24 21:44:12 -0300

    Inserindo Pedido

--- src/main/java/com/example/cursomc/domain/ItemPedido.java
+++ src/main/java/com/example/cursomc/domain/ItemPedido.java
@@ -67,6 +67,14 @@ public class ItemPedido implements Serializable {
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
 
 	@JsonIgnore
 	public Pedido getPedido() {
--- src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/example/cursomc/domain/PagamentoComBoleto.java
@@ -1,12 +1,16 @@
 package com.example.cursomc.domain;
 
 import java.util.Date;
+
 import javax.persistence.Entity;
+
 import com.example.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
